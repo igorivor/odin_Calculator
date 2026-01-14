810525a5078fd87f5311950de3b384da02ea8343
igorivor <igorrako69@gmail.com>
2026-01-14 13:36:18 +0100

    Edited my JS file: add function calculator that calls function for basic math based on operator we sand to calculator function

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ const div = ((num1, num2) => {
     }
     return num1/num2;
 });
+
+const calculator = function (num1, operator, num2){
+    if(operator === "+"){
+        return add(num1, num2);
+    }else if(operator === "-"){
+        return sub(num1, num2);
+    }else if(operator === "*"){
+        return mul(num1, num2);
+    }else if(operator === "/"){
+        return div(num1, num2);
+    }else {
+        return console.log("Error! Enter correct math operator!");
+    } 
+    
+}
