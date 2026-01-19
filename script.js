5fda173e4868022a7a9c19ea4386a885b8c60ca0
igorivor <igorrako69@gmail.com>
2026-01-19 14:07:47 +0100

    Edited JS file display now shows ERROR when tring to divied by 0!

--- script.js
+++ script.js
@@ -51,8 +51,8 @@ const add = ((num1, num2) => num1 + num2);
 const sub = ((num1, num2) => num1 - num2);
 const mul = ((num1, num2) => num1 * num2);
 const div = ((num1, num2) => {
-    if(num2 == 0){
-        return console.log("Error! Divided by 0!");
+    if(num2 === 0){
+        return display.value = "ERROR";
     }
     return num1/num2;
 });
